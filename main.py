6b9f18cb2aff3f3086271ec7b97028e463c9d63d
cryptovizor <smendoza171@gmail.com>
2021-03-31 15:58:57 -0700

    added daatabase app

--- main.py
+++ main.py
@@ -1,17 +1,21 @@
 from tkinter import *
 from PIL import ImageTk, Image
 from operator import itemgetter
+
 import tkinter.scrolledtext as st
 from tkinter import messagebox, filedialog
-import io
-import sys
-import os
+
+import io, sys, os
 from os import listdir
 from os.path import isfile, join
+
 import numpy as np
 import matplotlib.pyplot as plt
 
+import pygame
 
+import sqlite3
+db_name = 'address_book.db'
 #Change output to save to a variable instead of printing to screen
 #Changes output method and switches back after assigning
 def save_output():
@@ -43,7 +47,8 @@ def create_root():
     root = Tk()
     title_text = "TK Sesh"
     root.title(title_text)
-    center_window(root, 1500, 1000)
+    root.configure(bg='slategray4')
+    center_window(root, 800, 1000)
 
     #Change window icon
     p1 = PhotoImage(file='bitcoin.png')
@@ -79,6 +84,8 @@ def homepage(root):
     #Main dashboard, 5 rows x 2 columns
     dashboard = LabelFrame(root, text='Dashboard', padx=30, pady=20)
     dashboard.grid(row=1, column=0)
+    dashboard.configure(bg='slategray3')
+
     #Positioning inside frame is independent from root
 
 
@@ -90,7 +97,8 @@ def homepage(root):
 
     #APPS SECTION
     apps_frame = LabelFrame(dashboard, text='Apps', padx=generic_padx, pady=generic_pady)
-    apps_frame.grid(row=0, column=0)
+    apps_frame.grid(row=1, column=1)
+    apps_frame.configure(bg='slategray2')
 
     app_padx = 30
     app_pady = 10
@@ -108,22 +116,21 @@ def homepage(root):
 
 
     #INPUT
-    input_frame = LabelFrame(dashboard, text='Input Field', padx=generic_padx, pady=generic_pady)
-    input_frame.grid(row=0, column=1)
+    input_frame = LabelFrame(dashboard, text='Input Field', padx=generic_padx, pady=generic_pady, bg='slategray2')
+    input_frame.grid(row=1, column=2)
 
     Label(input_frame, text="Enter Text:").grid(row=0)
-    entry_field = StringVar()
-    entry = Entry(input_frame, textvariable=input_frame, width=50).grid(row=1, columnspan=2)
+    entry = Entry(input_frame, width=50)
+    entry.grid(row=1, columnspan=2)
+
     #entry.insert(1, "Enter a cryptocurrency...") #Placeholder text
-    global current
-    current = entry_field.get()
-    print(current)
-    Label(input_frame, text=current).grid(row=2)
+    input_text = entry.get()
+    Label(input_frame, text='').grid(row=4)
 
-    def myClick(text):
-        Label(input_frame, text=text).grid(row=4)
+    def display_input():
+        Label(input_frame, text=entry.get()).grid(row=4)
 
-    Button(input_frame, text="Click Here!", command=lambda: myClick(current)).grid(row=3)
+    Button(input_frame, text="Click Here!", command=display_input).grid(row=3)
 
 
 
@@ -133,7 +140,8 @@ def homepage(root):
 
     #RADIOS
     radio_frame = LabelFrame(dashboard, text='Radio Buttons', padx=generic_padx, pady=generic_pady)
-    radio_frame.grid(row=0, column=2)
+    radio_frame.grid(row=2, column=1)
+    radio_frame.configure(bg='slategray2')
 
     radios = [
         ("Option 1", 1),
@@ -164,7 +172,8 @@ def homepage(root):
 
     #SCROLL BAR
     scroll_frame = LabelFrame(dashboard, text='Output Console', padx=generic_padx, pady=generic_pady)
-    scroll_frame.grid(row=0, column=3)
+    scroll_frame.grid(row=3, column=2)
+    scroll_frame.configure(bg='slategray2')
 
     text_area = st.ScrolledText(scroll_frame, width=30, height=8, font=("Times New Roman", 15))
     text_area.grid(row=0, column=6, rowspan=10, pady=10, padx=10)
@@ -187,27 +196,28 @@ def homepage(root):
 
 
 
-    #BUTTONS FRAME
+    #BUTTON FRAME
     button_frame = LabelFrame(dashboard, text='Button Depot', padx=generic_padx, pady=generic_pady)
-    button_frame.grid(row=0, column=4)
+    button_frame.grid(row=2, column=2)
+    button_frame.configure(bg='slategray2')
 
     #Exit Button
-    Button(button_frame, text='Quit', command=root.quit).grid(row=0, column=0)
+    Button(button_frame, text='Quit', command=root.quit, bg='yellow').grid(row=0, column=1)
 
     button_padx = 30
     button_pady = 20
-    Button(button_frame, text='1', padx=button_padx, pady=button_pady).grid(row=1, column=0)
-    Button(button_frame, text='2', padx=button_padx, pady=button_pady).grid(row=1, column=1)
-    Button(button_frame, text='3', padx=button_padx, pady=button_pady).grid(row=1, column=2)
-    Button(button_frame, text='4', padx=button_padx, pady=button_pady).grid(row=2, column=0)
-    Button(button_frame, text='5', padx=button_padx, pady=button_pady).grid(row=2, column=1)
-    Button(button_frame, text='6', padx=button_padx, pady=button_pady).grid(row=2, column=2)
-    Button(button_frame, text='7', padx=button_padx, pady=button_pady).grid(row=3, column=0)
-    Button(button_frame, text='8', padx=button_padx, pady=button_pady).grid(row=3, column=1)
-    Button(button_frame, text='9', padx=button_padx, pady=button_pady).grid(row=3, column=2)
+    Button(button_frame, text='1', padx=button_padx, pady=button_pady, bg='red3').grid(row=1, column=0)
+    Button(button_frame, text='2', padx=button_padx, pady=button_pady, bg='red3').grid(row=1, column=1)
+    Button(button_frame, text='3', command=lambda: audio_buttons(), padx=button_padx, pady=button_pady, bg='green2').grid(row=1, column=2)
+    Button(button_frame, text='4', padx=button_padx, pady=button_pady, bg='red3').grid(row=2, column=0)
+    Button(button_frame, text='5', padx=button_padx, pady=button_pady, bg='red3').grid(row=2, column=1)
+    Button(button_frame, text='6', padx=button_padx, pady=button_pady, bg='red3').grid(row=2, column=2)
+    Button(button_frame, text='7', padx=button_padx, pady=button_pady, bg='red3').grid(row=3, column=0)
+    Button(button_frame, text='8', padx=button_padx, pady=button_pady, bg='red3').grid(row=3, column=1)
+    Button(button_frame, text='9', padx=button_padx, pady=button_pady, bg='red3').grid(row=3, column=2)
 
-
-    Button(button_frame, text="Broken", state=DISABLED).grid(row=6, column=0)
+    #Broken Button
+    Button(button_frame, text="Broken", state=DISABLED).grid(row=0, column=2)
 
     #MESSAGE BOXES
     # showinfo, showwarning, showerror, askquestion, askokcancel, askyesno
@@ -224,9 +234,9 @@ def homepage(root):
         else:
             Label(button_frame, text='Wrong Choice').grid(row=10, column=3)
 
-    Button(button_frame, text='INFO', command=info_popup).grid(row=10, column=0)
-    Button(button_frame, text='ERROR', command=error_popup).grid(row=10, column=1)
-    Button(button_frame, text='?', command=ask_popup).grid(row=10, column=2)
+    Button(button_frame, text='INFO', command=info_popup, bg='yellow').grid(row=10, column=0)
+    Button(button_frame, text='ERROR', command=error_popup, bg='orange').grid(row=10, column=1)
+    Button(button_frame, text='?', command=ask_popup, bg='yellow').grid(row=10, column=2)
 
 
 
@@ -239,7 +249,8 @@ def homepage(root):
     #FILE SEARCH FRAME
     #Will only return locatiom of file (c:\\ etc.)
     file_frame = LabelFrame(dashboard, text='File Search', padx=generic_padx, pady=generic_pady)
-    file_frame.grid(row=1, column=0)
+    file_frame.grid(row=1, column=3)
+    file_frame.configure(bg='slategray2')
 
     def open_explorer():
         root.filename = filedialog.askopenfilename(initialdir="/", title="Select A File", filetypes=(('png files', '*.png'), ('all files', '*.*')))
@@ -249,9 +260,11 @@ def homepage(root):
 
 
 
+
     #SLIDER FRAME
     slider_frame = LabelFrame(dashboard, text='Sliders', padx=generic_padx, pady=generic_pady)
-    slider_frame.grid(row=1, column=1)
+    slider_frame.grid(row=3, column=1)
+    slider_frame.configure(bg='slategray2')
 
     vh_sum = 0
     Label(slider_frame, text="X + Y = " + str(vh_sum)).grid(row=3, column=3)
@@ -279,22 +292,24 @@ def homepage(root):
 
     #CHECKBOXES FRAME
     checkbox_frame = LabelFrame(dashboard, text='Checkbox', padx=generic_padx, pady=generic_pady)
-    checkbox_frame.grid(row=1, column=2)
+    checkbox_frame.grid(row=2, column=3)
+    checkbox_frame.configure(bg='slategray2')
 
     checkboxes = [
-        ("Option 1", 1),
-        ("Option 2", 2),
-        ("Option 3", 3),
-        ("Option 4", 4),
-        ("Option 5", 5),
+        ('Python', 'Python'),
+        ('Java', 'Java'),
+        ('C++', 'C++'),
+        ('HTML', 'HTML'),
+        ('CSS', 'CSS')
     ]
 
-    mode = IntVar()
-    mode.set(0)
+    # mode = IntVar()
+    # mode.set(0)
 
     selected_checks = []
 
     def select_option(var):
+        print(selected_checks)
         if var in selected_checks:
             selected_checks.remove(var)
         else:
@@ -302,17 +317,21 @@ def homepage(root):
 
     def display_options():
         for item in selected_checks:
+            print(item)
+            print(selected_checks)
             Label(checkbox_frame, text=str(item)).grid(column=0)
 
 
-    check_var = IntVar()
+    check_var = StringVar()
 
     for text, value in checkboxes:
+        print(value)
+        checkbox = Checkbutton(checkbox_frame, text=text, variable=value, command=lambda: select_option(value), onvalue=value, offvalue='')
+        #checkbox = Checkbutton(checkbox_frame, text=text, variable=value, command=lambda: select_option(value), onvalue=1, offvalue=0)
 
-        checkbox = Checkbutton(checkbox_frame, text=text, variable=value, command=lambda: select_option(value), onvalue=1, offvalue=0)
         checkbox.grid(column=0)
 
-        checkbox.deselect()
+        #checkbox.deselect()
 
     Button(checkbox_frame, text='Show Selection', command=display_options).grid(row=len(checkboxes)+1)
 
@@ -325,7 +344,8 @@ def homepage(root):
 
     #DROPDOWN FRAME
     dropdown_frame = LabelFrame(dashboard, text='Dropdown', padx=generic_padx, pady=generic_pady)
-    dropdown_frame.grid(row=1, column=3)
+    dropdown_frame.grid(row=3, column=3)
+    dropdown_frame.configure(bg='slategray2')
 
     options = [
         'Monday',
@@ -545,7 +565,226 @@ def image_viewer_app():
 def database_app():
     top = Toplevel()
     top.title('Database')
-    center_window(top, 300, 300)
+    center_window(top, 400, 600)
+
+    # Connect to database and create cursor
+    conn = sqlite3.connect(db_name)
+    c = conn.cursor()
+
+
+    #Create Table
+    def create_address_table():
+        c.execute("""CREATE TABLE addresses (
+            first_name text,
+            last_name text,
+            address text,
+            city text,
+            state text,
+            zipcode integer
+            )""")
+    #create_address_table()
+
+    def delete():
+        conn = sqlite3.connect(db_name)
+        c = conn.cursor()
+
+        c.execute("DELETE from addresses WHERE oid=" + delete_box.get())
+
+        conn.commit()
+        conn.close()
+
+    def update():
+        conn = sqlite3.connect(db_name)
+        c = conn.cursor()
+
+        record_id = delete_box.get()
+        c.execute("""UPDATE addresses SET
+                    first_name = :first,
+                    last_name = :last,
+                    address = :address,
+                    city = :city,
+                    state = :state,
+                    zipcode = :zipcode
+                    
+                    WHERE oid = :oid""",
+        {
+            'first': f_name_editor.get(),
+            'last': l_name_editor.get(),
+            'address': address_editor.get(),
+            'city': city_editor.get(),
+            'state': state_editor.get(),
+            'zipcode': zipcode_editor.get(),
+
+            'oid': record_id
+        })
+
+        conn.commit()
+        conn.close()
+        editor.destroy()
+
+
+    def edit():
+        #Open editor in new window
+        global editor
+        editor = Toplevel()
+        editor.title('Update Record')
+        center_window(editor, 400, 300)
+
+        conn = sqlite3.connect(db_name)
+        c = conn.cursor()
+
+
+        record_id = delete_box.get()
+        c.execute("SELECT * FROM addresses WHERE oid = " + record_id)
+        records = c.fetchall()
+
+
+        global f_name_editor
+        global l_name_editor
+        global address_editor
+        global city_editor
+        global state_editor
+        global zipcode_editor
+
+
+        # Entry Fields
+        f_name_label = Label(editor, text="First Name").grid(row=0, column=0, pady=(10, 0))
+        f_name_editor = Entry(editor, width=30)
+        f_name_editor.grid(row=0, column=1, padx=20, pady=(10, 0))
+
+        l_name_label = Label(editor, text="Last Name").grid(row=1, column=0)
+        l_name_editor = Entry(editor, width=30)
+        l_name_editor.grid(row=1, column=1)
+
+        address_label = Label(editor, text="Address").grid(row=2, column=0)
+        address_editor = Entry(editor, width=30)
+        address_editor.grid(row=2, column=1)
+
+        city_label = Label(editor, text="State").grid(row=3, column=0)
+        city_editor = Entry(editor, width=30)
+        city_editor.grid(row=3, column=1)
+
+        state_label = Label(editor, text="City").grid(row=4, column=0)
+        state_editor = Entry(editor, width=30)
+        state_editor.grid(row=4, column=1)
+
+        zipcode_label = Label(editor, text="Zipcode").grid(row=5, column=0)
+        zipcode_editor = Entry(editor, width=30)
+        zipcode_editor.grid(row=5, column=1)
+
+
+
+        for record in records:
+            f_name_editor.insert(0, record[0])
+            l_name_editor.insert(0, record[1])
+            address_editor.insert(0, record[2])
+            city_editor.insert(0, record[3])
+            state_editor.insert(0, record[4])
+            zipcode_editor.insert(0, record[5])
+
+
+        # Save Button
+        save_button = Button(editor, text="Save Record", command=update)
+        save_button.grid(row=6, column=0, columnspan=2, padx=10, pady=10, ipadx=145)
+
+
+        conn.commit()
+        conn.close()
+
+    def submit():
+        conn = sqlite3.connect(db_name)
+        c = conn.cursor()
+        c.execute("INSERT INTO addresses VALUES (:f_name, :l_name, :address, :city, :state, :zipcode)",
+                  {
+                      'f_name': f_name.get(),
+                      'l_name': l_name.get(),
+                      'address': address.get(),
+                      'city': city.get(),
+                      'state': state.get(),
+                      'zipcode': zipcode.get()
+                  })
+        conn.commit()
+        conn.close()
+
+        f_name.delete(0, END)
+        l_name.delete(0, END)
+        address.delete(0, END)
+        city.delete(0, END)
+        state.delete(0, END)
+        zipcode.delete(0, END)
+
+    def query():
+        conn = sqlite3.connect(db_name)
+        c = conn.cursor()
+
+        c.execute("SELECT *, oid FROM addresses")
+        # c.fetchone()
+        records = c.fetchall()
+        # c.fetchmany(10)
+
+        print_records = ''
+        for record in records:
+            print_records += str(record[0]) + ' ' + str(record[1]) + ' \t' + str(record[6]) + '\n'
+
+        Label(top,text=print_records).grid(row=11, column=0, columnspan=2)
+
+        conn.commit()
+        conn.close()
+
+
+
+    #Entry Fields
+    f_name_label = Label(top, text="First Name").grid(row=0, column=0, pady=(10,0))
+    f_name = Entry(top, width=30)
+    f_name.grid(row=0, column=1, padx=20, pady=(10,0))
+
+    l_name_label = Label(top, text="Last Name").grid(row=1, column=0)
+    l_name = Entry(top, width=30)
+    l_name.grid(row=1, column=1)
+
+    address_label = Label(top, text="Address").grid(row=2, column=0)
+    address = Entry(top, width=30)
+    address.grid(row=2, column=1)
+
+    city_label = Label(top, text="State").grid(row=3, column=0)
+    city = Entry(top, width=30)
+    city.grid(row=3, column=1)
+
+    state_label = Label(top, text="City").grid(row=4, column=0)
+    state = Entry(top, width=30)
+    state.grid(row=4, column=1)
+
+    zipcode_label = Label(top, text="Zipcode").grid(row=5, column=0)
+    zipcode = Entry(top, width=30)
+    zipcode.grid(row=5, column=1)
+
+
+    delete_box_label = Label(top, text="Edit/Delete ID").grid(row=9, column=0, pady=5)
+    delete_box = Entry(top, width=30)
+    delete_box.grid(row=9, column=1, pady=5)
+
+
+
+    #Submit Button
+    submit_button = Button(top, text="Add Record to Database", command=submit)
+    submit_button.grid(row=6, column=0, columnspan=2, padx=10, pady=10, ipadx=100)
+
+    #Query Button
+    query_button = Button(top, text="Show Records", command=query)
+    query_button.grid(row=7, column=0, columnspan=2, padx=10, pady=10, ipadx=137)
+
+    #Delete Button
+    delete_button = Button(top, text="Delete Record", command=delete)
+    delete_button.grid(row=10, column=0, columnspan=2, padx=10, pady=10, ipadx=136)
+
+    #Edit Button
+    edit_button = Button(top, text="Edit Record", command=edit)
+    edit_button.grid(row=12, column=0, columnspan=2, padx=10, pady=10, ipadx=143)
+
+    #Commit and close connection
+    conn.commit()
+    conn.close()
+
 
 def weather_app():
     top = Toplevel()
@@ -565,6 +804,14 @@ def plotting_app():
         plt.show()
 
     Button(top, text="Plot Graph", command=graph).grid(row=5)
+
+
+def audio_buttons():
+    pygame.mixer.init()
+
+    pygame.mixer.music.load('audio.mp3')
+    pygame.mixer.music.play(loops=0)
+
 
 
 
